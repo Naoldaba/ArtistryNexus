0c69e808c8418f782b92566bd40179233fbafb70
KalebAsratemedhin <risekab@gmail.com>
2024-07-15 12:09:48 +0300

    setup frontend react app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Typography from '@mui/material/Typography'
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h1" color="initial"> Hello Artists</Typography>
+    </div>
+  );
+}
+
+export default App;
